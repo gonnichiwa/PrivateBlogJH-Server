aaf2ab58b6b8261fb32a6631586bdfd18189ec67
gonnichiwa <keepthefaith00@gmail.com>
2018-01-02 17:30:35 +0900

    - spring 4.2.1 release로 업데이트 (@CrossOrigin 테스트 및 숙지..)

--- src/main/java/privateBlog/gonnichiwa/sample/controller/SampleController.java
+++ src/main/java/privateBlog/gonnichiwa/sample/controller/SampleController.java
@@ -7,11 +7,13 @@ import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import privateBlog.gonnichiwa.sample.service.SampleService;
 
+@CrossOrigin
 @Controller
 public class SampleController {
 	
@@ -20,7 +22,7 @@ public class SampleController {
 	@Resource(name="sampleService")
 	private SampleService sService;
 	
-		
+//	@CrossOrigin 컨트롤러의 메소드에 달면 해당 요청만 CrossOrigin 처리
 	@RequestMapping(value="/sample/getMenuList.do")
 	public ModelAndView getMenuList(Map<String, Object> commandMap) throws Exception{
 		ModelAndView mav = new ModelAndView();
