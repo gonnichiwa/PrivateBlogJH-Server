a61d6e859bd5a0c7d4fbdc1abf3c120e82cbca9f
gonnichiwa <keepthefaith00@gmail.com>
2018-01-03 10:33:03 +0900

    테스트 쿼리 (LeftNavigationMenu List 오름차순 정렬)

--- src/sqlFiles/privateBlog_DML.sql
+++ src/sqlFiles/privateBlog_DML.sql
@@ -1,4 +1,4 @@
 
 -- 메뉴 불러오기
 
-select rownum, bm_idNum, bm_name,bm_lev, bm_step, bm_ref, bm_isDel from blogMenu;
+select rownum, bm_idNum, bm_name,bm_lev, bm_step, bm_ref, bm_isDel from blogMenu order by rownum asc;
